179e0bfcdf55c403cd35713bdb9ca857fdc3bc0a
pratham000112 <virmanipratham2@gmail.com>
2026-03-05 23:56:03 +0530

    type conversion

---
+++ 01_basics/03_typeconversion.js
@@ -0,0 +1,26 @@
+let score = "45abc";
+
+//console.log(typeof score)
+
+//console.log(typeof (score))
+
+let valueinNumber = Number(score)
+//console.log(typeof valueinNumber)
+
+//console.log(valueinNumber)
+
+// "33" => 33
+// "33abc" => Nan
+// true => 1; false => 0
+
+let boolenNumber = ""
+
+let isLogged = Boolean(boolenNumber)
+console.log(isLogged)
+
+
+// 1 = true
+// 0 = false
+// "0" = true
+// "1" = true
+//
