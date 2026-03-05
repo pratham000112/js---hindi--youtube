d41f040f39df4a333d96f54e5d21fd2f4ce81417
pratham000112 <virmanipratham2@gmail.com>
2026-03-05 15:36:51 +0530

    pratise of var

--- 02_practise.js/practisevar.js
+++ 02_practise.js/practisevar.js
@@ -8,3 +8,14 @@ let city = "gurugram";
 console.log(name)
 
 console.table([name, age, city]);
+
+// Swap two variables 
+
+let a = 10;
+let b = 20;
+
+let temp = a;
+ a = b;
+ b = temp;
+
+  console.log(a,b);
