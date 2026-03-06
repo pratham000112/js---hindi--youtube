82a806a17de1a1e38db51c25e87c0ca763509b01
pratham000112 <virmanipratham2@gmail.com>
2026-03-06 23:46:22 +0530

    practise conversion

---
+++ 02_practise.js/pratype.js
@@ -0,0 +1,51 @@
+// Ecommerce question practise 
+
+let price = "500";
+let quantity = 2;
+
+let total = Number(price) * quantity;
+
+console.log(total); // why it is taken as a string the reason is the j/s can taken in the real project as a string according to the Html code so that is why we have to do that for that.
+
+
+console.log("10"+2+3);
+
+console.log("10"+(2+3));// in this is the bracket they first add then convert
+
+console.log(Boolean("0"))
+
+console.log(Boolean(0))
+
+console.log(Number("100px"));
+
+// Advanced questions
+
+console.log(null+1) // 1
+
+console.log(undefined+1) // Nan
+
+console.log(true+true) //2
+
+console.log(true+false+1) // 2
+
+ console.log("5"+true) // 5true finding.
+
+ console.log("5"+false) // 
+
+ console.log([]+[]) // blank nothing it convert into the "" empty string if both are empty it show the nothing in it.
+
+ console.log([]+{}) // object and object
+
+ console.log({}+[]) // if the one vlaue is string the other is string in it.
+
+ console.log(Number("")) // 0
+
+ console.log(Number(" "))// first the j/s remove the empty brackets then convert this number into the 0
+
+ console.log(Boolean([])) //  this is the object so it comes as true.
+
+ console.log(Boolean({}))// {} this is the array so it comes the true in it some value are falty other are true
+
+ console.log([] ==  false); // the reason is that when we do that the [] convert into the Number so it becomes true the reason is (==) this operator
+ console.log("" == 0); //
+
