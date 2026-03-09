9851d925e6ce92503945d11fa78ab5a2b71e869b
pratham000112 <virmanipratham2@gmail.com>
2026-03-10 00:17:16 +0530

    dataType Summary

---
+++ 01_basics/05_datatypes_summary.js
@@ -0,0 +1,29 @@
+// Primitive
+
+// 7 types : string, Number, Boolean,null,undefined,Symbol,BigInt
+
+
+//javascript is a dynamically typed languages  this means we don't have the declare the data type of the variable.
+
+//Example
+let value = 10
+//console.log(typeof (value))
+
+value = "Hello"
+//console.log(typeof (value))
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+
+
+
+ 
+
+// Reference (Non primitive)
+
+//Array, objects,Functions
+
+
