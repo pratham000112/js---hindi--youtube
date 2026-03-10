64c04c81c46b175e0d8acd20dfc84e2a83cc9f94
pratham000112 <virmanipratham2@gmail.com>
2026-03-10 18:27:26 +0530

    function in j/s

---
+++ 01_basics/07_functions.js
@@ -0,0 +1,9 @@
+
+//functions are reausable block code designed to perform some particular tasks.
+
+function sayHello()  {
+    return "Hello World"
+}
+
+let message = sayHello()
+console.log(message)
