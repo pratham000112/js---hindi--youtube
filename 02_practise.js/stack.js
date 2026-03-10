143d422acb21b17fa7f9e556a7d51cfed67b0b52
pratham000112 <virmanipratham2@gmail.com>
2026-03-10 14:38:42 +0530

    practise stack

---
+++ 02_practise.js/stack.js
@@ -0,0 +1,11 @@
+// Practise questions
+
+let a = 20;
+let b = a;
+
+b = 40
+
+console.log(a);
+console.log(b); // it is the example of the stack in it.
+
+// Primitive value - Number , int , Boolean,  null, undefined,symbol in it.
