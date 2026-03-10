ba78ef573e66ab95c918c74d4113e3454f5fc0ce
pratham000112 <virmanipratham2@gmail.com>
2026-03-10 18:37:01 +0530

    function question

--- 01_basics/07_functions.js
+++ 01_basics/07_functions.js
@@ -1,9 +1,28 @@
 
 //functions are reausable block code designed to perform some particular tasks.
 
+// functions are defined with the function keyword followed by the parathanses.
+     
 function sayHello()  {
     return "Hello World"
 }
 
 let message = sayHello()
 console.log(message)
+
+//Local Variable
+ // variable decalred with the j/s function , become local to the function.
+ // the code inside the function is not executed when the function is defined.
+ 
+
+ // Parameters functions
+ // Parameters allow you to pass or send the value to a function.
+   // listed inside the paranthesis  in the function defination in it.
+
+
+   function  tocelsius(fahrenheit) {
+      return (5/9) * (fahrenheit-32)
+   }
+
+   let value = tocelsius(77)
+   console.log(value)   
