b6bb7011b76ce47edf955146d62bb292001651f2
pratham000112 <virmanipratham2@gmail.com>
2026-03-11 23:26:38 +0530

    Array intro

---
+++ 02_basics/01_array.js
@@ -0,0 +1,17 @@
+// array.
+
+const myArr = [10,20,30,40,50,60]
+ console.log(myArr)
+
+ console.log(myArr[0])
+
+ // Array is that enable storing a collection of multiple items  under a single variable names, array can store the any data type in it.
+ 
+ // Example of array.
+// it is the like the basket . it contains many fruits but  the basket is the one object in it.
+
+
+
+
+
+ 
