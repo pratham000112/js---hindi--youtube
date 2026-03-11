0a4eb9614e9db2be9b2932eaa3a5b52f4a352963
pratham000112 <virmanipratham2@gmail.com>
2026-03-11 11:51:08 +0530

    method of string

--- 01_basics/09_Numbermath.js
+++ 01_basics/09_Numbermath.js
@@ -14,15 +14,7 @@ console.log(hundreds.toLocaleString('en-In'));
 
 
 
-const strPrim = "foo"
-const  strPrim2 = String(1)
-const strPrim3 = String(true)
-const obj = new String(strPrim);// it is cause the unexpected behavior .
 
-console.log(typeof strPrim)
-console.log(typeof strPrim2);
-console.log(typeof strPrim3);
-console.log(typeof  obj)  // string cannot convert the type of the object in it.
 
 
 
