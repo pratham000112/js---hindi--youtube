9d88e8d2c4bdd79f031a7f5914a535a27ae1eb4c
pratham000112 <virmanipratham2@gmail.com>
2026-03-17 03:15:38 +0530

    Function in Objects

--- 02_basics/02_objects.js
+++ 02_basics/02_objects.js
@@ -1,11 +1,12 @@
 // object literals
 
-const mysym = "MyKey1"
+const mysym =Symbol("MyKey1")
 
 const CricketPlayers = {
      name : "Abhishek sharma",
       age : 27,
       location : "Punjab",
+      [mysym] : "MyKey1",
       email :"Abhisharma123@gmail.com",
       isLoggedIn: false,
       LastLoginDays : ["Monday","Saturday"]
@@ -13,6 +14,33 @@ const CricketPlayers = {
 
 console.log(CricketPlayers.isLoggedIn)  //  this is the one method of doing that
 console.log(CricketPlayers["location"]) //  this is the another method of that 
+
+console.log(CricketPlayers[mysym])
+
+// interview question use symbol as a key.
+ 
+CricketPlayers.email = "Abhisharma123@yopmail.com";
+//console.log(CricketPlayers);
+//Object.freeze(CricketPlayers)
+//CricketPlayers.email = "Abhisharma1234@yopmail.com"
+//console.log(CricketPlayers)
+
+CricketPlayers.greeting = function() {
+    console.log("i am opening Batsman");
+}
+
+CricketPlayers.greetingTwo = function() {
+    console.log(`${this.name} is the Great Power Hitter`)
+}
+
+
+
+
+console.log(CricketPlayers.greeting());
+console.log(CricketPlayers.greetingTwo());
+
+ 
+
 
 
 
