bee7701eb4e3f09a1595a2b3efbf4e02f73f0391
pratham000112 <virmanipratham2@gmail.com>
2026-03-16 10:27:00 +0530

    Object in j/s

---
+++ 02_basics/02_objects.js
@@ -0,0 +1,18 @@
+// object literals
+
+const mysym = "MyKey1"
+
+const CricketPlayers = {
+     name : "Abhishek sharma",
+      age : 27,
+      location : "Punjab",
+      email :"Abhisharma123@gmail.com",
+      isLoggedIn: false,
+      LastLoginDays : ["Monday","Saturday"]
+}
+
+console.log(CricketPlayers.isLoggedIn)  //  this is the one method of doing that
+console.log(CricketPlayers["location"]) //  this is the another method of that 
+
+
+
