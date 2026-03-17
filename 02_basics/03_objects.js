aedde8d361efc5cfa1f0dbfc7b34cbac24724544
pratham000112 <virmanipratham2@gmail.com>
2026-03-17 23:28:26 +0530

    Object part 2

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -5,7 +5,7 @@ Freemeetup.id = "123ab"
 Freemeetup.name = "Kush"
 Freemeetup.isLoggedIn = false
 
-console.log(Freemeetup);
+//console.log(Freemeetup);
 
 
 const regularUser = {
@@ -18,4 +18,29 @@ const regularUser = {
         }
     }
 
-    console.log(regularUser.fullname)
+    //console.log(regularUser.fullname)
+
+    const obj1 = {1:"a", 2: "b"}
+    const obj2 = {3: "a", 4: "b"}
+    
+
+    //const obj3 = Object.assign({}, obj1, obj2, )  //  this is the first syntax
+
+    //console.log(obj3);
+
+    //const obj3 = {...obj1, ...obj2} // this is the Second Syntax of mergening them in it.
+      //console.log(obj3)
+
+
+      console.log(Freemeetup);
+
+      console.log(Object.keys(Freemeetup));
+
+      console.log(Object.values(Freemeetup));
+
+      console.log(Object.entries(Freemeetup));
+
+      console.log(Freemeetup.hasOwnProperty('Age'));
+
+
+
