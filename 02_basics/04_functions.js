0acb0a91760a771ac4b3bfb68f89ac87b2cde4dd
pratham000112 <virmanipratham2@gmail.com>
2026-03-20 07:57:35 +0530

    function in j/s

---
+++ 02_basics/04_functions.js
@@ -0,0 +1,53 @@
+
+function sayHello (){
+    console.log("p");
+    console.log("u");
+    console.log("l");
+    console.log("k");
+    console.log("i");
+    console.log("t");
+}
+
+sayHello()
+
+
+// function addTwoNumbers(number1, number2) {
+//     console.log(number1 + number2);
+// }
+
+// addTwoNumbers(2,2)
+
+// function addTwoNumbers(number1, number2) {
+//     let result = number1 + number2
+//       return result
+// }
+
+// const result = addTwoNumbers(3,5)
+
+// console.log("Result", result);
+
+function addTwoNumbers(number1, number2){
+       return number1 * number2
+}
+
+const result = addTwoNumbers(5,5)
+   console.log("Result",result);
+
+
+function loginUserMessage(username) {
+    if(!username) {
+        console.log("Please enter a username");
+         return 
+    }
+       return `${username} just logged in`
+}
+                
+console.log(loginUserMessage())
+
+//console.log(loginUserMessage("Harshita"))
+
+//backsticks is used for the templeate literals it is used to  insert variable inside the string
+
+
+
+
---
+++ 02_practise.js/objects.js
@@ -0,0 +1,11 @@
+
+const user = {
+  name: "Pratham",
+  age: 21
+};
+
+console.log(Object.keys(user));  //  name , age
+
+console.log(Object.values(user));  //pratham , 21
+
+console.log(Object.entries(user));
