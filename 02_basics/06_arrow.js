e715c87bcd6239bebdfb080b1119fcbb76d30509
pratham000112 <virmanipratham2@gmail.com>
2026-03-25 22:59:14 +0530

    this keyword

---
+++ 02_basics/06_arrow.js
@@ -0,0 +1,48 @@
+
+
+// const user = {
+//     username: "Pulkit virmani",
+//     price:999,
+
+//     welcomeMessage: function() {
+//         console.log(`${this.username}, welcome to website`);
+//         console.log(this);
+
+//     }
+// }
+
+
+// user.welcomeMessage()
+//  user.username = "Harshita"
+//  user.welcomeMessage()
+
+//  console.log(this);
+
+
+//  const code =function () {
+//            let coffee = "High caffiene"
+//        console.log(this.coffee);
+//  }
+
+//  const Player  = () => {
+//     let username = "Pulkit virmani"
+//       console.log()
+//  }
+
+
+ 
+//  code();
+
+
+//  const addTwo = (num1, num2) => {
+//       return num1 + num2
+//  } // it is the Explicit conversion
+
+const addTwo  = (num1, num2) => num1 + num2;  // it is the implicit conversion in it
+
+const addThree = (num1, num2, num3) => num1 + num2 + num3; // it is the implicit conversin  in it
+ console.log(addTwo(10,8,10))
+
+
+
+    
