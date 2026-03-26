3957e3b9047ab1bc7310c0916c8680a93bdd2633
pratham000112 <virmanipratham2@gmail.com>
2026-03-26 18:00:58 +0530

    iife

---
+++ 02_basics/07_iife.js
@@ -0,0 +1,14 @@
+//immediatly invoked function 
+// Afunction that run immediately invoked
+
+
+(function chai(){
+    console.log('DB CONNECTED');
+})(); 
+
+
+(function code(){
+      console.log('i am the coder');
+})();
+
+
