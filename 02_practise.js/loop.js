5e4123fddee4b7589b48479a82d1f02974f81bf8
pratham000112 <virmanipratham2@gmail.com>
2026-03-29 22:14:15 +0530

    practise of Loop

--- 02_practise.js/loop.js
+++ 02_practise.js/loop.js
@@ -31,6 +31,33 @@ let i = 1;
          console.log(`Sum of number is ${sum}`)
  
    // Reverse Counting While loop
+        
+   
+   // Reverse a number.
+let Number = 10;
 
-    
+while (Number >= 1)
+{
+   console.log(Number);
+     Number--;
+}
+
+
+  // Print Hello Five times
+
+//   let message = 1
+
+//   for(let message =1; message <=5; message ++)
+//     {
+//        console.log("Hello");
+//     }
   
+
+    // same question using  the do- While loop
+
+   let P = 1;
+
+   do {
+         console.log("Hello");
+            P = P+1;
+   }  while(P <=5);
