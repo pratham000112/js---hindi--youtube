d75640f5539735072fd9f7279b988b43245f43dc
pratham000112 <virmanipratham2@gmail.com>
2026-03-29 07:46:43 +0530

    loop

---
+++ 02_basics/08_loops.js
@@ -0,0 +1,10 @@
+
+
+  for(let i = 0; i<=10; i++) {
+       const element = i;
+       if (element === 8) {
+          console.log("8  is my number");
+       } 
+
+       console.log(element);
+  }
