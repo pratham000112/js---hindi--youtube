625dba1ac9b62ec1f9406981aac1c8043c5a244c
pratham000112 <virmanipratham2@gmail.com>
2026-03-29 14:45:26 +0530

    loop j/s

---
+++ 02_practise.js/loop.js
@@ -0,0 +1,36 @@
+// Print Number 1 to 10
+
+let i = 1;
+
+// for(let i = 1; i <= 10; i++)
+//  {
+//     const number = i
+//       console.log(number);
+//  }
+
+
+ // Even numbers  from 1 to 20
+   // let number = 1   
+
+   //   for(let number = 1; number<=20;number++) {
+   //        if (number %2 === 0){
+   //          console.log(number)
+
+   //        }
+   //   }
+
+     // Sum of Numbers
+       let H = 1
+       let sum = 0
+   
+         while(H <=10){
+              sum = sum + H;
+                H++;
+         }
+
+         console.log(`Sum of number is ${sum}`)
+ 
+   // Reverse Counting While loop
+
+    
+  
