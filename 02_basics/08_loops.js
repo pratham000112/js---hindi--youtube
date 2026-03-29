6608f4195c1e57ee2712db33bd056606f650d5cc
pratham000112 <virmanipratham2@gmail.com>
2026-03-29 08:19:29 +0530

    array loop

--- 02_basics/08_loops.js
+++ 02_basics/08_loops.js
@@ -5,6 +5,22 @@
        if (element === 8) {
           console.log("8  is my number");
        } 
-
-       console.log(element);
+                                 
+       console.log(element);   // for or if 
   }
+
+
+//   for(let i = 1; i <=10; i++) {
+//       console.log(`outer loop values: ${i}`);
+//     for(let j = 1 ; j <=10; j++) {
+//         console.log(`${i} * ${j} = ${i * j}`);  // for - for
+//     }
+//   }
+
+let myArray = ["flash","batman","superman"]
+   console.log(myArray.length);
+for (let index = 0; index < myArray.length; index++) {
+     const element = myArray[index]
+      console.log(element);   // Array for Loop
+      
+}
