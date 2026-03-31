adc83525ceeb83c4c39465043eb64c12d6b46e40
pratham000112 <virmanipratham2@gmail.com>
2026-03-31 23:15:37 +0530

    reduce

--- 02_practise.js/mapfilter.js
+++ 02_practise.js/mapfilter.js
@@ -49,6 +49,22 @@ const NewNums = myNumbers
 
                 console.log(NewNums);
 
+
+const NewNumber = [1,2,3]
+
+// const myTotal = NewNumber.reduce(function (acc, currval) {
+//        console.log(`acc: ${acc} and currval: ${currval}`);
+//           return acc + currval
+// }, 0)
+
+//  console.log(myTotal)
+
+ const myTotal  = NewNumber.reduce( (acc, curr) => acc+curr, 0)
+
+ console.log(myTotal);
+
+
+
  
 
 
