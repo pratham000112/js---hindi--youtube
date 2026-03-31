680a0870e774290653d4ea897ae8caa3a15a1a15
pratham000112 <virmanipratham2@gmail.com>
2026-03-31 22:51:02 +0530

    filter and Map

--- 02_practise.js/mapfilter.js
+++ 02_practise.js/mapfilter.js
@@ -29,5 +29,30 @@ const  soldbooks = books.filter(  (product) => {
 
 //console.log(soldbooks)  // filter is used to filter all the values it does not want to print in it.
 
-console.log(booksYear)
+// console.log(booksYear)
+
+
+
+
+
+const myNumbers  =  [1,2,3,4,5,6,7,8,9,10]
+
+//  const NewNums = myNumbers.map( (num) => {return num + 10})
+//   console.log(NewNums);   // when used the scope it is necessary to use the return Keyword in it
+
+
+const NewNums = myNumbers
+                .map((num) => num *  10)
+                .map((num => num +1))
+                .filter((num) => num >=40)
+
+
+                console.log(NewNums);
+
+ 
+
+
+
+
+
 
