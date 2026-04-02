cc0489e98c400a3a0a48fec64b6b7554efd680c7
pratham000112 <virmanipratham2@gmail.com>
2026-04-02 09:27:06 +0530

    double array

--- 02_practise.js/mappractise.js
+++ 02_practise.js/mappractise.js
@@ -3,4 +3,18 @@
 
 const numbers = [2,3,4,5,6,7,8]
 
-const myNum = numbers.filter(numbers)
+const myNum = numbers.filter(  (nums) => {
+          return    nums % 2 !== 0
+})
+ 
+console.log(myNum)  // very good
+
+// given an array where each number is doubled in it.
+
+const arr = [2,4,6,8]
+
+const newarr = arr.map( (doublearr)  => {
+    return doublearr  * 2
+});
+
+console.log(newarr);
